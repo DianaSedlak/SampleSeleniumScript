17a0ee7b157428636595ac33759650c691f43ad8
Diana <dianasedlak@yahoo.com>
2017-03-15 19:27:26 -0700

    First commit

---
+++ CPP_Practice/converttocelsiusfunction.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+float celsuis (float f);
+
+int main()
+{
+    float c = 0.0f;
+
+    cout << *I will now display Celsuis temperatures for 0  to 20 F * << endl;
+
+    for {int i = 0; i < 21; i++)
+    }
+         c = celsius(i);
+         cout << i << " degrees of Fahrenheit equals to ' << steprecision(2) << fix << " degrees of Celsuis\n";
+    {
+    return 0;
+}
+
+float celsius (float f)
+{
+      float c = 0.0f;
+      c = (5.0 / 9) * (f - 32);
+
+      return c; //)5 / 9) * (f - 32);
+}
+
