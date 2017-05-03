97fa8be5bcacbd1883d03caef56495f25c876df6
Diana Sedlak <dianasedlak@yahoo.com>
2017-05-03 12:06:36 -0700

    Update and rename Science37_Selenium_Script.py to Sample_Selenium_Script.py

--- Python_Practice/Sample_Selenium_Script.py
+++ Python_Practice/Sample_Selenium_Script.py
@@ -13,11 +13,11 @@ driver.get("http://www.google.com")
 search_field = driver.find_element_by_id("lst-ib")
 search_field.clear()
 
-# enter the name of a really close startup that could save the lives of those I love, and it could also blend my past of dropping out of grad school for Occupational Therapy with my last three years of software testing and tinkering with technology and mobile. Did I mention this cuts my current commute in half? Woohooo I could walk to work! Wait do they have onsite showers? So not professional, but this script is.
-search_field.send_keys("Science 37")
+# Enter my name
+search_field.send_keys("Diana Sedlak")
 search_field.submit()
 
-# See how awesome and close Science 37 is, but whats with the 984 phone number?
+# See what comes up
 # By geting the list of elements which are displayed after the search
 #currently on result page using find_elements_by_class_name method
 lists=driver.find_elements_by_class_name("_Rm")
@@ -25,8 +25,7 @@ lists=driver.find_elements_by_class_name("_Rm")
 # get the number of elements found
 print ("Found " + str(len(lists))+ "searches;")
 
-# iterate through each element and print the text... Wait wow impressive Series B round!
-
+# iterate through each element and print the text
 i=0
 for listitem in lists:
     print (listitem)
@@ -34,6 +33,6 @@ for listitem in lists:
     if(i>10):
        break
 
-#close the browser window and hope this is a decent cover letter
+#close the browser window 
 driver.quit()
  
